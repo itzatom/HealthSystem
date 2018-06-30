8e742089a6a34635724d8df06247cc0500bb5019
Ubuntu <ubuntu@ip-172-31-26-122.us-west-2.compute.internal>
2018-06-30 16:37:13 +0000

    edit init_postgresql_sh

--- db/postgresql/init_postgresql.sh
+++ db/postgresql/init_postgresql.sh
@@ -2,9 +2,14 @@
 
 #   Create user
 sudo -u postgres createuser dbahealthsystem -d -E
-sudo -u postgres createdb HealthSystem -O dbahealthsystem
+sudo -u postgres createdb healthsystem -O dbahealthsystem
 sudo -i -u postgres psql -c "alter USER dbahealthsystem WITH PASSWORD 'healthsystem'"
 
+sudo -u postgres createuser medico -D -E
+sudo -i -u postgres psql -c "alter USER medico WITH PASSWORD 'passwdmedico'"
 
-sudo -u postgres psql -d HealthSystem -U dbahealthsystem -W -f sqlfiles/postgres_ddl.sql
-sudo -u postgres psql -d HealthSystem -U dbahealthsystem -W -f sqlfiles/postgres_dcl.sql 
+sudo -u postgres createuser paziente -D -E
+sudo -i -u postgres psql -c "alter USER paziente WITH PASSWORD 'passwdpaziente'"
+
+sudo -u postgres psql -d healthsystem -U dbahealthsystem -W -f sqlfiles/postgres_ddl.sql
+sudo -u postgres psql -d healthsystem -U dbahealthsystem -W -f sqlfiles/postgres_dcl.sql 
