2e19051f4afd7109f7ace88ea7ebb222637c023a
Ubuntu <ubuntu@ip-172-31-26-122.us-west-2.compute.internal>
2018-06-30 22:45:57 +0000

    fixed date format

--- db/postgresql/init_postgresql.sh
+++ db/postgresql/init_postgresql.sh
@@ -15,14 +15,15 @@ sudo -i -u postgres psql -c "alter USER medico WITH PASSWORD 'passwdmedico'"
 sudo -u postgres createuser paziente -D -E
 sudo -i -u postgres psql -c "alter USER paziente WITH PASSWORD 'passwdpaziente'"
 
-#		Change date format from MDY (Month/Day/Year) to DMY (Day/Month/Year)
-sudo -i -u postgres psql -c "ALTER DATABASE 'healthsystem' SET datestyle TO 'ISO, DMY'"
+#   Change date format from MDY (Month/Day/Year) to DMY (Day/Month/Year)
+sudo -i -u postgres psql -c "ALTER DATABASE healthsystem SET datestyle TO 'ISO, DMY'"
+sudo service postgresql restart 
 
 sudo -u postgres psql -d healthsystem -U dbahealthsystem -W -f sqlfiles/postgres_ddl.sql
 sudo -u postgres psql -d healthsystem -U dbahealthsystem -W -f sqlfiles/postgres_dcl.sql
 sudo -u postgres psql -d healthsystem -U dbahealthsystem -W -f sqlfiles/postgres_dml.sql
 
 
-#	Command for run PostgreSQL command line
+#   Command for run PostgreSQL command line
 sudo -u postgres psql -d healthsystem -U dbahealthsystem -W 
 
