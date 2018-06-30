85cef150c808cf0cf9173580ee01a82d575831a2
Antonio Mentone <antonio.mentone.96@gmail.com>
2018-07-01 00:14:47 +0200

    added populating of persona table

--- db/postgresql/sqlfiles/postgres_dml.sql
+++ db/postgresql/sqlfiles/postgres_dml.sql
@@ -1,10 +1,10 @@
 --  Insert on table TIPO_DOC
---  DEFAULT is for serial id_tipo
+--  DEFAULT is for Serial id_tipo
 insert into tipo_doc values (DEFAULT, 'passaporto');
 insert into tipo_doc values (DEFAULT, 'carta di identita');
 
 --  Insert on table INDIRIZZO
---  DEFAULT is for serial id_indirizzo
+--  DEFAULT is for Serial id_indirizzo
 insert into indirizzo values (DEFAULT, '80121', 'Via Gaetani 22');
 insert into indirizzo values (DEFAULT, '80127', 'Via Cilea 250');
 insert into indirizzo values (DEFAULT, '80123', 'Via Alessandro Manzoni 141');
@@ -66,21 +66,58 @@ insert into indirizzo values (DEFAULT, '80100', 'Via Salv. Rosa 146');
 insert into indirizzo values (DEFAULT, '80127', 'Via Tasso 183');
 insert into indirizzo values (DEFAULT, '80100', 'Via Renato Carosone 39');
 insert into indirizzo values (DEFAULT, '80129', 'Via Kerbaker 8');
-insert into indirizzo values (DEFAULT, '80133', 'Via Robeerto Bracco 71');
-insert into indirizzo values (DEFAULT, '80127', 'Via Achille Vianelli 10');
-insert into indirizzo values (DEFAULT, '80126', E'Via Dell\'Epomeo 219');
-insert into indirizzo values (DEFAULT, '80144', 'Corso Secondigliano 166');
-insert into indirizzo values (DEFAULT, '80126', 'Via Garzilli 59');
-insert into indirizzo values (DEFAULT, '80137', 'Piazza Cavour 19');
-insert into indirizzo values (DEFAULT, '80131', 'Via A. De Martino 7');
-insert into indirizzo values (DEFAULT, '80129', 'Via G. Santacroce 5');
-insert into indirizzo values (DEFAULT, '80129', 'Via S. Abate 33');
-insert into indirizzo values (DEFAULT, '80138', 'Via Duomo 19');
-insert into indirizzo values (DEFAULT, '80122', 'Via Arangio Ruiz 83');
-insert into indirizzo values (DEFAULT, '80143', 'Via Rimini 67');
-insert into indirizzo values (DEFAULT, '80129', 'Via Eduardo Suarez 16');
-insert into indirizzo values (DEFAULT, '80129', 'Viale Michelangelo 13');
-insert into indirizzo values (DEFAULT, '80128', E'Piazza Medaglie D\'Oro 27');
-insert into indirizzo values (DEFAULT, '80122', 'Via Luca Da Penne 3');
-insert into indirizzo values (DEFAULT, '80134', 'Via Toledo 373');
 
+--  Insert on table PERSONA
+--  DEFAULT is for Serial id_persona
+insert into persona values (DEFAULT, 'Estelle',  'Ballintime',   'eballintime0',    'tecweb', 'ZXVDRH50G03Q083B', 1,  'Ngembo',          '21/08/1992');
+insert into persona values (DEFAULT, 'Verna',    'Langforth',    'vlangforth1',     'tecweb', 'VFJRKG48F53C062E', 2,  'Sintansin',       '15/02/1987');
+insert into persona values (DEFAULT, 'Earl',     'Tansill',      'etansill2',       'tecweb', 'RCTMKH30N80T085O', 3,  'Julcamarca',      '20/03/1954');
+insert into persona values (DEFAULT, 'Jerrilyn', 'Checketts',    'jchecketts3',     'tecweb', 'PYAZGH27I98S624X', 4,  'Borgo Maggiore',  '29/08/1991');
+insert into persona values (DEFAULT, 'Cinda',    'Kearford',     'ckearford4',      'tecweb', 'KQDHSE54X52Y594U', 5,  'Härnösand',       '01/05/1978');
+insert into persona values (DEFAULT, 'Ashely',   'Van Haeften',  'avanhaeften5',    'tecweb', 'MXSNCO55I77Y025P', 6,  'Zselickislak',    '25/07/1996');
+insert into persona values (DEFAULT, 'Piper',    'Bullivent',    'pbullivent6',     'tecweb', 'ZVUKAY72J31Y534N', 7,  'Rossosh’',        '03/09/1994');
+insert into persona values (DEFAULT, 'Merna',    'Dorber',       'mdorber7',        'tecweb', 'SXQOWC26T27C805M', 8,  'Dongyuan',        '22/11/1973');
+insert into persona values (DEFAULT, 'Sheffy',   'Zanetti',      'szanetti8',       'tecweb', 'TZAVWM91R87L472Q', 9,  'Łąck',            '09/10/1960');
+insert into persona values (DEFAULT, 'Chickie',  'Flounders',    'cflounders9',     'tecweb', 'INWRAQ02T39Q546W', 10, 'Changchi',        '06/11/1965');
+insert into persona values (DEFAULT, 'Lora',     'Quiddinton',   'lquiddintona',    'tecweb', 'FHILPZ80P08S497T', 11, 'Tashtagol',       '06/11/1955');
+insert into persona values (DEFAULT, 'Benedikt', 'Rivilis',      'brivilisb',       'tecweb', 'FJKPGO17K63A671X', 12, 'Gävle',           '16/12/1996');
+insert into persona values (DEFAULT, 'Emmalyn',  'Jantel',       'ejantelc',        'tecweb', 'YFNKLI30T15W331L', 13, 'Zhvanets',        '02/07/1963');
+insert into persona values (DEFAULT, 'Joellyn',  'Caldaro',      'jcaldarod',       'tecweb', 'GPEAIM50P04L102M', 14, 'Dulian',          '22/12/1998');
+insert into persona values (DEFAULT, 'Jabez',    'Hovy',         'jhovye',          'tecweb', 'NICLRO57T31X853J', 15, 'Muzhijie',        '30/03/1978');
+insert into persona values (DEFAULT, 'Shelagh',  'Payley',       'spayleyf',        'tecweb', 'MPINAR88J74C464A', 16, 'Luoyang',         '17/07/1963');
+insert into persona values (DEFAULT, 'Allyn',    'Dunnet',       'adunnetg',        'tecweb', 'SRUJBC25P72A954T', 17, 'Sefrou',          '01/11/1965');
+insert into persona values (DEFAULT, 'Martainn', 'McMearty',     'mmcmeartyh',      'tecweb', 'YRIUQL81O89N542W', 18, 'Malakand',        '09/10/1972');
+insert into persona values (DEFAULT, 'Sorcha',   'Skellon',      'sskelloni',       'tecweb', 'EAIBKO15D97X162S', 19, 'Livádion',        '29/06/1978');
+insert into persona values (DEFAULT, 'Egan',     'Hansmann',     'ehansmannj',      'tecweb', 'WYZJEV14W79P978O', 20, 'Haduohe',         '11/11/1984');
+insert into persona values (DEFAULT, 'Vera',     'Volett',       'vvolettk',        'tecweb', 'RVEUFC50S12A446S', 21, 'Lokea',           '27/07/1968');
+insert into persona values (DEFAULT, 'Hadria',   'Weetch',       'hweetchl',        'tecweb', 'TYSADJ96S91B360M', 22, 'Zbąszynek',       '29/11/1954');
+insert into persona values (DEFAULT, 'Neville',  'Jacklings',    'njacklingsm',     'tecweb', 'DUIJHY72L41Y289Q', 23, 'Chengjiao',       '20/01/1972');
+insert into persona values (DEFAULT, 'Marjorie', 'Filliskirk',   'mfilliskirkn',    'tecweb', 'YPKTUZ37G67G518P', 24, 'Xiadahe',         '11/11/1995');
+insert into persona values (DEFAULT, 'Skipper',  'Flips',        'sflipso',         'tecweb', 'RFLIVS34Y24Q150L', 25, 'Berëzovyy',       '02/05/1980');
+insert into persona values (DEFAULT, 'Jozef',    'Trimmill',     'jtrimmillp',      'tecweb', 'YCEPHT93Z54F292W', 26, 'Shuangkou',       '16/01/1988');
+insert into persona values (DEFAULT, 'Charlton', 'Lill',         'clillq',          'tecweb', 'IGERWH50D04A258O', 27, 'Padilla',         '23/09/1991');
+insert into persona values (DEFAULT, 'Land',     'Gresch',       'lgreschr',        'tecweb', 'NWCMGD99O61Y478G', 28, 'Sanjinquan',      '02/12/1950');
+insert into persona values (DEFAULT, 'Dallis',   'Lomas',        'dlomass',         'tecweb', 'MDYRAL60Y83K340M', 29, 'Samamiya',        '30/07/1994');
+insert into persona values (DEFAULT, 'Domingo',  'Betchley',     'dbetchleyt',      'tecweb', 'MRWKBS71F17I176C', 30, 'Killumney',       '12/09/1995');
+insert into persona values (DEFAULT, 'Morey',    'McColley',     'mmccolleyu',      'tecweb', 'CBQKMD57W74X329E', 31, 'Chefchaouene',    '23/11/1954');
+insert into persona values (DEFAULT, 'Wiley',    'Vickar',       'wvickarv',        'tecweb', 'ODIKZU13E00G734M', 32, 'Medicine Hat',    '11/08/1978');
+insert into persona values (DEFAULT, 'Erwin',    'Mara',         'emaraw',          'tecweb', 'TFRCVQ83E39Z999L', 33, 'Rāwandūz',        '14/08/1997');
+insert into persona values (DEFAULT, 'Cristy',   'Phil',         'cphilx',          'tecweb', 'XQJOMU84O38R417O', 34, 'Long Beach',      '28/06/1951');
+insert into persona values (DEFAULT, 'Antonius', 'Franke',       'afrankey',        'tecweb', 'IQUAJX39K52Q229Z', 35, 'Ar Ru’ays',       '22/07/1996');
+insert into persona values (DEFAULT, 'Worthy',   'Meeny',        'wmeenyz',         'tecweb', 'QNTOWY11C74I873R', 36, 'Sinazongwe',      '01/09/1963');
+insert into persona values (DEFAULT, 'Alfonso',  'Mellsop',      'amellsop10',      'tecweb', 'SFEPJB93P26H465V', 37, 'Dzhankoy',        '29/03/1960');
+insert into persona values (DEFAULT, 'Danika',   'Schimke',      'dschimke11',      'tecweb', 'HABEKR88T19O046R', 38, 'Trondheim',       '10/09/1973');
+insert into persona values (DEFAULT, 'Virgie',   'Golledge',     'vgolledge12',     'tecweb', 'LRIYKZ93T65T215I', 39, 'Mekkaw',          '18/11/1962');
+insert into persona values (DEFAULT, 'Leonelle', 'Stickells',    'lstickells13',    'tecweb', 'JZUQOL90O82D975I', 40, 'Nyeri',           '28/07/1994');
+
+-- The last ten are medical, theirs id must be added to medico table on foreign key id_medico
+insert into persona values (DEFAULT, 'Ray',      'Herrieven',    'rherrieven14',    'tecweb', 'YISBWL00Y98B090H', 41, 'Kitakyūshū',      '07/11/1953');
+insert into persona values (DEFAULT, 'Ban',      'Lantiff',      'blantiff15',      'tecweb', 'EPWTNL70D93W099M', 42, 'Tinalmud',        '03/01/1971');
+insert into persona values (DEFAULT, 'Gwenny',   'Menichi',      'gmenichi16',      'tecweb', 'IWTYHQ94D79A579U', 43, 'Majayjay',        '18/04/1978');
+insert into persona values (DEFAULT, 'Donni',    'Windram',      'dwindram17',      'tecweb', 'URJIBW27X96N355B', 44, 'Tangxi',          '01/05/1963');
+insert into persona values (DEFAULT, 'Lavinia',  'Teffrey',      'lteffrey18',      'tecweb', 'GBHFKO31G98H247E', 45, 'San Agustín',     '23/07/1953');
+insert into persona values (DEFAULT, 'Eadmund',  'Steely',       'esteely19',       'tecweb', 'UITCLX67S27A755N', 46, 'Igbo Ora',        '19/10/1962');
+insert into persona values (DEFAULT, 'Way',      'Wortt',        'wwortt1a',        'tecweb', 'OQLTIB84R35K196V', 47, 'Motala',          '18/11/1975');
+insert into persona values (DEFAULT, 'Erie',     'Krysztowczyk', 'ekrysztowczyk1b', 'tecweb', 'STONBM52I08O505Y', 48, 'As Suqaylibīyah', '22/03/1978');
+insert into persona values (DEFAULT, 'Augusto',  'Bent',         'abent1c',         'tecweb', 'JSBUWM76J37T412W', 49, 'Göteborg',        '06/06/1959');
+insert into persona values (DEFAULT, 'Mattheus', 'Puddicombe',   'mpuddicombe1d',   'tecweb', 'FQEKNJ96J37E931X', 50, 'Batangtoru',      '26/02/1979');
