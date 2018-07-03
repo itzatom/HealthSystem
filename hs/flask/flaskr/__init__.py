6ab902b3952e0f3bc113a8c03eae43a63d3c8017
itzatom <vittorio.fones@gmail.com>
2018-07-03 22:06:51 +0200

    added nginx folder with config, updated docker-compose and testing integration with flask. Still not comunicating

--- hs/flask/flaskr/__init__.py
+++ hs/flask/flaskr/__init__.py
@@ -4,10 +4,11 @@ from .db.models import Persona
 
 app = Flask(__name__)
 
-@app.route ("/")
+@app.route("/nginxflask", methods=('GET', 'POST'))
 def index():
-    test = db_session.query(Persona.nome).all() 
-    return str(test) 
+    if request.method == 'POST':
+        test = db_session.query(Persona.nome).all() 
+        return str(test) 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
