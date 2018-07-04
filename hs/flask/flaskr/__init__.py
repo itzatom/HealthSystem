4d8949e54e742054cabc57b1c0e622fc9189ae45
itzatom <vittorio.fones@gmail.com>
2018-07-04 14:16:19 +0200

    Still not reverse proxying to flask from nginx (Maybe macOSX bug?)

--- hs/flask/flaskr/__init__.py
+++ hs/flask/flaskr/__init__.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 from .db.db import engine, db_session
 from .db.models import Persona
 
 app = Flask(__name__)
 
-@app.route("/nginxflask", methods=('GET', 'POST'))
+@app.route("/hs/nginxflask", methods=('GET', 'POST'))
 def index():
     if request.method == 'POST':
         test = db_session.query(Persona.nome).all() 
         return str(test) 
+    else:
+        return "<h1> no POST method </h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
