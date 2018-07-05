ddf9886a00664e9586be2375d9784942268d312f
itzatom <vittorio.fones@gmail.com>
2018-07-05 03:10:24 +0200

    alternatives solution to proxy redirect because nginx couldn't render index html...

--- hs/flask/flaskr/__init__.py
+++ hs/flask/flaskr/__init__.py
@@ -1,8 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from .db.db import engine, db_session
 from .db.models import Persona
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
 
 @app.route("/hs", methods=('GET', 'POST'))
 def index():
