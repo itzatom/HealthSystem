2b4bc278b9007272e00124e90a2a6842747c8fc2
itzatom <vittorio.fones@gmail.com>
2018-07-05 23:41:29 +0200

    added wekzeug security function for checking db users password

--- hs/flask/flaskr/sql/models.py
+++ hs/flask/flaskr/sql/models.py
@@ -2,6 +2,9 @@
 from sqlalchemy import CheckConstraint, Column, Date, ForeignKey, Integer, String, Table, Time, text
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -41,6 +44,15 @@ class Persona(Base):
     indirizzo = relationship('Indirizzo', uselist=False)
     stud_leg = relationship('StudLeg', secondary='medico')
 
+    def __init__(self, username, password):
+        self.username = username
+        self.set_password(password)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 class StudLeg(Base):
     __tablename__ = 'stud_leg'
