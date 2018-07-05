33d2d5f620d319c432b95efc25c4479c28300e6a
Antonio Mentone <antonio.mentone.96@gmail.com>
2018-07-05 15:58:44 +0200

    flask dir fixed

--- hs/flask/flaskr/__init__.py
+++ hs/flask/flaskr/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, render_template
 from .db.db import engine, db_session
 from .db.models import Persona
-from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route("/")
 def home():
