aed451be341cd371d25034a8e9df2b97c491f914
Antonio Mentone <antonio.mentone.96@gmail.com>
2018-07-07 20:47:33 +0200

    add view for patient info

--- hs/flask/flaskr/views.py
+++ hs/flask/flaskr/views.py
@@ -31,9 +31,10 @@ def login():
     return render_template('index.html')
 
 
-@app.route('/hs/patient_info')
-def patient_info(username):
-    return str(username)
+@app.route('/hs/info/<username>')
+def info(username):
+    p = Persona.query.filter_by(username=username).first()
+    return render_template('homepage/info.html', patient=p)
 
 @app.route('/hs/doctor/<_username>', methods=['GET','POST'])
 @login_required
