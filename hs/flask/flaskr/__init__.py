6debbb768069ce80c79cb597b5c51121b7516014
Antonio Mentone <antonio.mentone.96@gmail.com>
2018-07-07 17:25:08 +0200

    added login handler, fixed login view

--- hs/flask/flaskr/__init__.py
+++ hs/flask/flaskr/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, render_template, Blueprint
-from .sql.db_config import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_required
 
 app = Flask(__name__)
+app.secret_key = 'our secret key'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dbahealthsystem:passwddba@postgres:5432/healthsystem'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from .sql import models
 from . import error_handler
 from . import views
 
