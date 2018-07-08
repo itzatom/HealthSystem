ee0342c466513f0f420c0eab793895e03992bc6a
Antonio Mentone <antonio.mentone.96@gmail.com>
2018-07-08 16:02:53 +0200

    restyling doctor.html, now the doctor can remove his patient

--- hs/flask/flaskr/views.py
+++ hs/flask/flaskr/views.py
@@ -1,9 +1,9 @@
-from flaskr import app
+from flaskr import app, db
 from flaskr import login_manager
 from flask import url_for, session, redirect, request, render_template, flash
 from flask_login import login_user, login_required, logout_user
 from .sql.models import Persona
-from .sql.models import Medico, Paziente
+from .sql.models import Medico, Paziente, Ricetta
 
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'login'
@@ -41,6 +41,11 @@ def logout():
 def unauthorized_handler():
     return 'Unauthorized'
 
+
+
+
+#DOCTOR
+""" Get doctor homepage """
 @app.route('/hs/doctor/<_username>', methods=['GET','POST'])
 @login_required
 def doctor(_username):
@@ -50,13 +55,44 @@ def doctor(_username):
         patients = (Paziente.query.filter_by(id_medico=doc.id_medico).all())
         return render_template('homepage/doctor.html', doctor=doc, users=patients);
 
-@app.route('/hs/info/<username>')
-def info(username):
-    p = Persona.query.filter_by(username=username).first()
+""" Get info of a patient """
+@app.route('/hs/info/<p_username>')
+@login_required
+def info(p_username):
+    p = Persona.query.filter_by(username=p_username).first()
     return render_template('homepage/info.html', patient=p)
 
+""" Add a new patient """
+@app.route('/hs/newpatient', methods=['GET', 'POST'])
+@login_required
+def add_patient(m_username):
+    if request.method == 'GET':
+        return render_template('homepage/register.html')
+
+
+
+""" Remove a patient choise """
+@app.route('/hs/remove/<p_username>', methods=['GET'])
+@login_required
+def remove_patient(p_username):
+     p = Persona.query.filter_by(username=p_username).first()
+     p_info = Paziente.query.filter_by(id_paziente=p.id_persona).first()
+     med = p_info.medico
+
+     try:
+         db.session.query(Ricetta).filter(Ricetta.id_paziente==p_info.id_paziente).delete()
+         db.session.delete(p_info)
+         db.session.delete(p)
+         db.session.commit()
+     except:
+         db.session.rollback()
+
+     return redirect(request.args.get('next') or url_for('doctor', _username=med.persona.username))
+
+
+#PATIENT
 @app.route('/hs/patient/<username>', methods=['GET','POST'])
 @login_required
-def patient(_username):
+def patient(username):
     if request.method == 'GET':
         return render_template('homepage/patient.html', Persona=Persona);
