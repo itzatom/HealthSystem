b648392e6a05899f07d89fe6af34d041cec55312
Antonio Mentone <antonio.mentone.96@gmail.com>
2018-07-13 11:27:49 +0200

    first row of patient homepage completed: now patient can see prescription and doctor's profile

--- hs/flask/flaskr/views.py
+++ hs/flask/flaskr/views.py
@@ -63,7 +63,7 @@ def edit_profile(username):
 
         a=request.form['form-pass']
         b=request.form['conf-form-pass']
-        
+
         #Check if password has changed
         if a is not None and b is not None:
             if a==b and len(a) > 8:
@@ -385,3 +385,12 @@ def patient(_username):
         persona = Persona.query.filter_by(username=_username).first()
         paziente = Paziente.query.filter_by(id_paziente=persona.id_persona).first()
         return render_template('homepage/patient.html', paziente=paziente);
+
+""" Get prescription """
+@app.route('/hs/patient/prescription/<p_username>')
+@login_required
+def get_prescription(p_username):
+    p = Persona.query.filter_by(username=p_username).first()
+    r = Ricetta.query.filter_by(id_paziente=p.id_persona).all()
+
+    return render_template('homepage/read_prescription.html', prescription=r)
